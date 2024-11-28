09ac93b1a4daa6b70aa07ef7a79135101d034f06
Brenton Maddocks <Legelowes@gmail.com>
2024-11-28 10:59:58 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using FLORENCE;
+
+namespace FLORENCE
+{
+    public class Program
+    {
+        private static FLORENCE.framework framework;
+
+        public static void Main(String[] args)
+        {
+            System.Console.WriteLine("FLORENCE START");
+            framework = new FLORENCE.framework();
+            while (framework == null) { /* wait untill created */ }
+            framework.Get_Client().Get_Execute().Create_And_Run_Graphics();
+        }
+
+        public static FLORENCE.framework Get_Framework()
+        {
+            return framework;
+        }
+    }
+}
